4b24a639092c3dcdb40488e4c45f6aa4504e4b6e
Dean Winchester <59614870+DeanWinchester01@users.noreply.github.com>
2025-09-02 10:11:21 +1200

    reverted playerdata

--- Assets/Player_Characters/Scripts/PlayerData.cs
+++ Assets/Player_Characters/Scripts/PlayerData.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
-
+[CreateAssetMenu(fileName = "NewPlayer", menuName = "Player")]
 public class PlayerData: ScriptableObject
 {
     public string playerName;
+    [HideInInspector]
     public float health;
     public float maxHealth;
+    [HideInInspector]
     public float stamina;
     public float maxStamina;
     public int dmgMultiplier;

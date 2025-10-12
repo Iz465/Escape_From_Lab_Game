e2dfda88cf17a6221d29b48d5cc9849f592bc932
Dean Winchester <59614870+DeanWinchester01@users.noreply.github.com>
2025-10-13 11:29:17 +1300

    display door interact based on distance

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -1,16 +1,42 @@
+using System.Collections;
 using UnityEngine;
 
 public class OpenDoor : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Transform interact;
+    Transform plr;
     void Start()
     {
-        
+        interact = transform.Find("Canvas");
+        StartCoroutine(WaitForPlayer());
+    }
+
+    IEnumerator WaitForPlayer()
+    {
+        while (!GameObject.FindGameObjectWithTag("Player"))
+        {
+            yield return null;
+        }
+        if (GameObject.Find("Camera"))
+        {
+            Destroy(GameObject.Find("Camera"));
+        }
+
+        plr = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void DisplayInteraction()
+    {
+        if((plr.position - transform.position).magnitude < 10)
+            interact.gameObject.SetActive(true);
+        else
+            interact.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (plr == null) return;
+        DisplayInteraction();
     }
 }

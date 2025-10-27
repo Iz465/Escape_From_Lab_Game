29601499b326cd54fee12c3305c4a54d03937c89
Aedan H.P. Nicholls <99621280+Aedan-Paltridge-Nicholls@users.noreply.github.com>
2025-10-27 22:16:34 +1300

    More work to be done in FirePlayerInputs.cs

--- Assets/Player_Characters/Fire_Character/Scripts/FirePlayerInputs.cs
+++ Assets/Player_Characters/Fire_Character/Scripts/FirePlayerInputs.cs
@@ -19,6 +19,7 @@ public class FirePlayerInputs : MonoBehaviour
     [Range(0f,350f)]
     private float FireBall_Speed;
 
+    private string animationName = "Attack_1";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -30,11 +31,30 @@ public class FirePlayerInputs : MonoBehaviour
         Debug.Log("Attack 1 Input Received");
         if (!context.performed) return;
         animator.SetTrigger("Attack_1");
+
+        while (true)
+        {
+            AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+            
+            AnimationClip clip = animator.runtimeAnimatorController.animationClips[0];
+            if (clip.name == animationName)
+            {
+                float normalizedTime = stateInfo.normalizedTime % 1; // Get normalized time (0 to 1)
+                int currentFrame = Mathf.FloorToInt(normalizedTime * clip.frameRate * clip.length);
+
+                Debug.Log($"Current Frame: {currentFrame}");
+            }
+            
+        }
+
+        
         GameObject fireball = Instantiate(FireballPrefab, FiringPoint.position, Quaternion.identity);
         Vector3 movementDirection = controller.transform.position += FiringPoint.position;
+        
         movementDirection.y = FiringPoint.position.y;
         Vector3 force = new(movementDirection.x * 10f, movementDirection.y, movementDirection.z * 10f);
         fireball.GetComponent<Rigidbody>().AddRelativeForce(force);
+        GameObject.Destroy(fireball,7F);
     }
     public void Attack_2_AreaBlast(InputAction.CallbackContext context)
     {

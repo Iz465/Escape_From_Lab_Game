e958bf502a83af85bb0e6afd01abc2b94cf97e2a
Aedan H.P. Nicholls <99621280+Aedan-Paltridge-Nicholls@users.noreply.github.com>
2025-10-27 14:15:45 +1300

    Camera Fixed and movement done

--- Assets/Player_Characters/Fire_Character/Scripts/FirePlayerMovement.cs
+++ Assets/Player_Characters/Fire_Character/Scripts/FirePlayerMovement.cs
@@ -12,36 +12,58 @@ public class FirePlayerMovement : MonoBehaviour
     [Header("Character Stats Scriptable Object")]
     public LyraVeyne lyraVeyne;
     public float GracePeriod = 0.2f;
+    /*Movement Vars*/
     private float originalStepOffset;
     private float ySpeed;
-    public float gravity = -9.81f;
+    [Header("Jump Adjustments")]
     public float jumpSpeed;
-    public float HorazontaljumpSpeed;
+    public float HorizontalSpeed = 3f;
     private float? LastOnGroundTime;
     private float? LastJumpTime;
+    [Header("Stamina Costs")]
+    public float StaminaDrain_Sprint = 10;
+    public float StaminaDrain_Jump = 10;
+    public float StaminaRegen_Moving = 1.5f;
+    public float StaminaRegen_Idle = 3f;
+    
+    /*Movement Animators*/
     private bool isJumping;
     private bool isGrounded;
-    private bool isFalling;
-    private bool isSprinting;
-    
+    private float InputMagnitude;
+
     private void Start()
     {
         originalStepOffset = controller.stepOffset;
         animator = GetComponent<Animator>();
+        lyraVeyne.ResetStamina();
     }
     private void Update()
     {
-        float maximumSpeed = lyraVeyne.sprintSpeed;
+        
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         Vector3 movementDirection = new Vector3(horizontal, 0, vertical);
-        float InputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
+        InputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
+
+        
 
         if (Input.GetKey(KeyCode.LeftShift)|| Input.GetKey(KeyCode.RightShift))
         {
-            InputMagnitude /= 2;
-        }        
+            InputMagnitude /= 3;
+        }
+        else if (Input.GetKey(KeyCode.LeftControl)|| Input.GetKey(KeyCode.RightControl))
+        {
+            lyraVeyne.ReduceStamina(5f * Time.deltaTime);
+            if (lyraVeyne.Stamina <= 0) 
+            {
+                InputMagnitude /= 3;
+            }
+        }
+        else
+        {
+            InputMagnitude = (InputMagnitude /= 3)*2;
+        }
         animator.SetFloat("InputMagnitude", InputMagnitude, 0.05f, Time.deltaTime);
         
        // movementDirection = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up) * movementDirection;
@@ -67,11 +89,12 @@ public class FirePlayerMovement : MonoBehaviour
             animator.SetBool(name: "IsJumping", false);
             isJumping = false;
             animator.SetBool(name: "IsFalling", false);
-            isFalling = false;
-            if (Time.time - LastJumpTime <= GracePeriod)
+            
+            if (Time.time - LastJumpTime <= GracePeriod && lyraVeyne.Stamina >= 15f)
             {
                 ySpeed = jumpSpeed;
                 animator.SetBool(name: "IsJumping", true);
+                lyraVeyne.ReduceStamina(15f);
                 isJumping = true;
                 LastJumpTime = null;
                 LastOnGroundTime = null;
@@ -85,7 +108,7 @@ public class FirePlayerMovement : MonoBehaviour
             if ((isJumping && ySpeed < 0 ) || ySpeed<-2)
             {
                 animator.SetBool(name: "IsFalling", true);
-                isFalling = true;
+                
             }
         }
 
@@ -94,17 +117,19 @@ public class FirePlayerMovement : MonoBehaviour
         if (movementDirection != Vector3.zero)
         {
             animator.SetBool("IsMoving", true);
+            lyraVeyne.IncreaseStamina(.5f * Time.deltaTime);
             Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
             controller.transform.rotation = Quaternion.RotateTowards(controller.transform.rotation, toRotation, lyraVeyne.rotationSpeed * Time.deltaTime);
         }
         else
         {
+            lyraVeyne.IncreaseStamina(2f * Time.deltaTime);
             animator.SetBool("IsMoving", false);
         }
 
         if (isGrounded == false)
         {
-            Vector3 velocity = movementDirection * InputMagnitude * HorazontaljumpSpeed;
+            Vector3 velocity = HorizontalSpeed * InputMagnitude * movementDirection;
             velocity.y = ySpeed;
             controller.Move(velocity * Time.deltaTime);
 
--- Assets/Player_Characters/Fire_Character/Scripts/Lyra Veyne.cs
+++ Assets/Player_Characters/Fire_Character/Scripts/Lyra Veyne.cs
@@ -9,7 +9,7 @@ public class LyraVeyne : ScriptableObject
     public string characterName = "Lyra Veyne";
 
     [Range(0, 100)]
-    public int health;
+    public float health;
     [Range(0, 100)]
     public float Stamina;
 
@@ -21,7 +21,59 @@ public class LyraVeyne : ScriptableObject
 
 
     public string DisplayStamina;
-
+    public string DisplayHealth;
+    public void ResetStamina()
+    {
+        Stamina = 100f;
+        DisplayStamina = Stamina.ToString("F0");
+    }
+    public void IncreaseStamina(float amount)
+    {
+        Stamina += amount;
+        if (Stamina > 100)
+        {
+            Stamina = 100;
+        }
+        DisplayStamina = Stamina.ToString("F0");
+    }
+    public void ReduceStamina(float amount)
+    {
+        Stamina -= amount;
+        if (Stamina < 0)
+        {
+            Stamina = 0;
+        }
+        UpdateDisplayValues();
+    }
+    public void Resethealth()
+    {
+        health = 100f;
+        UpdateDisplayValues();
+    }
+    public void IncreaseHealth(float amount)
+    {
+        health += amount;
+        if (health > 100)
+        {
+            health = 100;
+        }
+        UpdateDisplayValues();
+    }
+    public void ReduceHealth(float amount)
+    {
+        health -= amount;
+        if (health < 0)
+        {
+            health = 0;
+        }
+        UpdateDisplayValues();
+    }
+    
+    public void UpdateDisplayValues()
+    {
+        DisplayStamina = Stamina.ToString("F0");
+        DisplayHealth = health.ToString("F0");
+    }
 }
 
 
